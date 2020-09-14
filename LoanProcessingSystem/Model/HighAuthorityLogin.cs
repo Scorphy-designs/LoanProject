21f67ebc6b771a99ee0932727db3c3bc39cb48a7
Nikhil Kumar Menaria <nikhilkumar.menaria@dxc.com>
2020-09-14 20:56:23 +0530

    Add project files.

---
+++ LoanProcessingSystem/Model/HighAuthorityLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoanProcessingSystem.Model
+{
+    public class HighAuthorityLogin
+    {
+
+        [Display(Name = "Email Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserName is required")]
+        public string EmailId { get; set; }
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
